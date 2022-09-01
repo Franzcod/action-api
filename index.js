4e8327a25e9b58ed91b316e86dd41c5131077ce5
FranciscoChattigo <fpalacios@chattigo.com>
2022-09-01 17:19:02 -0300

    fix

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const github = require("@actions/github");
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
+  const data = core.getInput("data");
+
   console.log(`Hello ${nameToGreet}!`);
+  console.log(`Data >>  ${data}!`);
+
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  // console.log(`The event payload: ${payload}`);
-  console.log(`>>> ${github.event.client_payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
