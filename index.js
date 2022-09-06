0ed028af96fb4f639ad21274b2ecf3fb588add90
FranciscoChattigo <fpalacios@chattigo.com>
2022-09-06 12:46:04 -0300

    fix 1246

--- index.js
+++ index.js
@@ -4,21 +4,11 @@ const data_managment = require("./data_managment");
 
 async function main() {
   try {
-    // `who-to-greet` input defined in action metadata file
-    // let PATH = core.getInput("PATH");
-    // let METHOD = core.getInput("METHOD");
-    // let EXP_RES = core.getInput("EXP_RES");
-    // let HEADERS = core.getInput("HEADERS");
-    // let PAYLOAD = core.getInput("PAYLOAD");
-
-    let PATH = "```\nhttps://jsonplaceholder.typicode.com/todos/1\n";
-    let METHOD = "GET\n";
-    let EXP_RES =
-      '\n{\n    "userId": 1,\n    "id": 1,\n    "title": "delectus aut autem",\n    "completed": false\n}\n';
-    let HEADERS =
-      '\n{\n  "Content-type": "application/json; charset=UTF-8"\n}\n```';
-    let PAYLOAD =
-      '\n{\n    "title": "foo",\n    "body": "bar",\n    "userId": 1\n}\n';
+    let PATH = core.getInput("PATH");
+    let METHOD = core.getInput("METHOD");
+    let EXP_RES = core.getInput("EXP_RES");
+    let HEADERS = core.getInput("HEADERS");
+    let PAYLOAD = core.getInput("PAYLOAD");
 
     PATH = data_managment.clean_data(PATH).toLowerCase();
     METHOD = data_managment.clean_data(METHOD).toLowerCase();
@@ -36,16 +26,23 @@ async function main() {
       "COMPARATION => ",
       JSON.stringify(datos.data) == JSON.stringify(EXP_RES)
     );
+
+    let status_code = datos.status;
+    let api_response = datos.data;
     comparation = JSON.stringify(datos.data) == JSON.stringify(EXP_RES);
 
+    core.setOutput("status_code", status_code);
+    core.setOutput("api_response", api_response);
     core.setOutput("comparation", comparation);
+
     // Get the JSON webhook payload for the event that triggered the workflow
     // Obtenga payload webhook del el evento que desencadenó el workflow
     // let payload = JSON.stringify(github.context.payload, undefined, 2);
   } catch (error) {
     core.setFailed(error.message);
-    comparation = false;
-    core.setOutput("comparation", comparation);
+    core.setOutput("status_code", "-");
+    core.setOutput("api_response", "-");
+    core.setOutput("comparation", false);
   }
 }
 
