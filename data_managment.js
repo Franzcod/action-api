cb04c92addad93afbf48c17ed3e5630690bb8d34
FranciscoChattigo <fpalacios@chattigo.com>
2022-09-06 10:49:08 -0300

    fix

--- data_managment.js
+++ data_managment.js
@@ -1,8 +1,38 @@
+const axios = require("axios");
+
 function clean_data(data) {
-  console.log(data);
-  console.log(typeof data);
+  let dato = data.split("");
+  let resp = "";
+
+  dato.forEach((element) => {
+    if (element !== "\n" && element !== "`") {
+      resp += element;
+    }
+  });
+
+  return resp;
+}
+
+async function getDataApi(path, method, header, payload) {
+  try {
+    const config = {
+      method,
+      url: path,
+      header,
+      payload,
+    };
+
+    let res = await axios(config);
+    return res;
+  } catch (err) {
+    return {
+      err,
+      msg: "Ocurrio un error en la peticion",
+    };
+  }
 }
 
 module.exports = {
   clean_data,
+  getDataApi,
 };
--- index.js
+++ index.js
@@ -2,25 +2,46 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const data_managment = require("./data_managment");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const PATH = core.getInput("PATH");
-  const METHOD = core.getInput("METHOD");
-  const EXP_RES = core.getInput("EXP_RES");
-  const HEADERS = core.getInput("HEADERS");
-  const PAYLOAD = core.getInput("PAYLOAD");
+async function main() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    // let PATH = core.getInput("PATH");
+    // let METHOD = core.getInput("METHOD");
+    // let EXP_RES = core.getInput("EXP_RES");
+    // let HEADERS = core.getInput("HEADERS");
+    // let PAYLOAD = core.getInput("PAYLOAD");
 
-  data_managment.clean_data(PATH);
-  data_managment.clean_data(METHOD);
-  data_managment.clean_data(EXP_RES);
-  data_managment.clean_data(HEADERS);
-  data_managment.clean_data(PAYLOAD);
+    let PATH = "```\nhttps://jsonplaceholder.typicode.com/todos/1\n";
+    let METHOD = "GET\n";
+    let EXP_RES =
+      '\n{\n    "userId": 1,\n    "id": 1,\n    "title": "delectus aut autem",\n    "completed": false\n}\n';
+    let HEADERS =
+      '\n{\n  "Content-type": "application/json; charset=UTF-8"\n}\n```';
+    let PAYLOAD =
+      '\n{\n    "title": "foo",\n    "body": "bar",\n    "userId": 1\n}\n';
 
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // Obtenga payload webhook del el evento que desencadenó el workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-} catch (error) {
-  core.setFailed(error.message);
+    PATH = data_managment.clean_data(PATH).toLowerCase();
+    METHOD = data_managment.clean_data(METHOD).toLowerCase();
+    EXP_RES = JSON.parse(data_managment.clean_data(EXP_RES));
+    HEADERS = JSON.parse(data_managment.clean_data(HEADERS));
+    PAYLOAD = JSON.parse(data_managment.clean_data(PAYLOAD));
+
+    let datos = await data_managment.getDataApi(PATH, METHOD, HEADERS, PAYLOAD);
+
+    console.log("DATOS       => ", datos.data);
+    console.log("EXP_RES     => ", EXP_RES);
+    console.log(
+      "COMPARATION => ",
+      JSON.stringify(datos.data) == JSON.stringify(EXP_RES)
+    );
+
+    // core.setOutput("comparation", comparation);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    // Obtenga payload webhook del el evento que desencadenó el workflow
+    // let payload = JSON.stringify(github.context.payload, undefined, 2);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+main();
